f8564502c935de525614e8a66f1ceacadbb74671
yafet-123 <yafetaddisu123@gmail.com>
2023-12-10 05:43:25 -1200

    edited z common hero section of skills

--- components/skills/CommonHeroSection.jsx
+++ components/skills/CommonHeroSection.jsx
@@ -25,19 +25,19 @@ const CommonHeroSection = ({background,Tagline,Welcome_Message}) => {
           </h1>
           <h1 className="flex flex-col text-xl text-center md:text-2xl mb-5 leading-10">
               <span className="mb-5">
-                Hello, I'm Yafet Addisu, a dedicated website developer with a passion for crafting seamless and innovative digital experiences. 
+                {`Hello, I'm Yafet Addisu, a dedicated website developer with a passion for crafting seamless and innovative digital experiences. 
                 My skill set encompasses a robust understanding of front-end technologies, including HTML5, CSS3, and JavaScript. I specialize 
                 in building responsive and user-friendly interfaces, and I'm proficient in utilizing popular frameworks like React.js for 
-                dynamic web applications.
+                dynamic web applications.`}
               </span>
               <span className="mb-5">
-                On the back end, I have hands-on experience with Node.js and Express, allowing me to create robust and scalable server-side 
-                solutions. I'm also adept at version control with Git and collaboration using platforms like GitHub.
+                {`On the back end, I have hands-on experience with Node.js and Express, allowing me to create robust and scalable server-side 
+                solutions. I'm also adept at version control with Git and collaboration using platforms like GitHub.`}
               </span>
               <span className="mb-5">
-                With a keen eye for design and a commitment to staying updated on the latest industry trends, I strive to deliver web solutions 
+                {`With a keen eye for design and a commitment to staying updated on the latest industry trends, I strive to deliver web solutions 
                 that not only meet technical requirements but also exceed user expectations. I am excited about the opportunity to contribute 
-                my skills to your projects and bring your ideas to life. Let's build something amazing together!
+                my skills to your projects and bring your ideas to life. Let's build something amazing together!`}
               </span>
           </h1>
       </div>

4b6cf694ed96f7079e8040050f4d576f970b58d8
yafet-123 <yafetaddisu123@gmail.com>
2023-12-14 15:06:00 -1200

    edited z project page

--- components/project/project.jsx
+++ components/project/project.jsx
@@ -46,10 +46,10 @@ const Project = ({project})=>{
                 </div>
                 <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
                     <div className='p-2'>
-                        <p className='text-center font-bold pb-2'>Technologies</p>
+                        <p className='text-center font-bold pb-2 text-lg lg:text-xl'>Technologies</p>
                         <div className='grid grid-cols-3 md:grid-cols-1'>
                             { technology?.map((tech,index)=>(
-                               <p key={index} className='text-gray-600 py-2 flex items-center'>
+                               <p key={index} className='text-gray-600 py-2 flex items-center text-md lg:text-lg'>
                                     <RiRadioButtonFill className='pr-1' /> {tech}
                                 </p> 
                             ))}

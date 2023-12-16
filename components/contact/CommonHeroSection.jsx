8b41893c11ec23be3448f8dbe04d8ae5c4e1ef33
yafet-123 <yafetaddisu123@gmail.com>
2023-12-15 14:14:30 -1200

    edited z contact for mobile responsive

--- components/contact/CommonHeroSection.jsx
+++ components/contact/CommonHeroSection.jsx
@@ -20,8 +20,8 @@ const CommonHeroSection = ({background,Tagline,Welcome_Message,photos}) => {
   const [paddingTop, setPaddingTop] = useState("0");
   return (
     <div className={`flex flex-col lg:flex-row h-full pt-28 md:px-10 bg-zinc-300`}>
-      <div className="text-black flex flex-col justify-center font-normal text-left w-full lg:w-[50%] mb-5 lg:mb-0 px-5">
-          <h1 className="text-2xl md:text-4xl mb-5 text-center md:text-left ">
+      <div className="text-black flex flex-col justify-center font-normal text-left w-full lg:w-[50%] mb-5 lg:mb-0 px-2 lg:px-5">
+          <h1 className="font-bold text-2xl md:text-4xl mb-5 text-center md:text-left ">
             {Tagline}
           </h1>
           <h1 className="text-xl text-center md:text-left md:text-2xl mb-5 leading-10">
@@ -32,7 +32,7 @@ const CommonHeroSection = ({background,Tagline,Welcome_Message,photos}) => {
       <div className="w-full lg:w-1/2 px-2">
         <Slider {...settings}>
           {photos.map((data, index) => (
-            <div key={index} className="relative h-96 lg:h-[450px] ">
+            <div key={index} className="relative h-[200px] lg:h-[450px] ">
               <Image
                 layout="fill"
                 src={data.image}

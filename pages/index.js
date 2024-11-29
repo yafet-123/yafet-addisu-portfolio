47997d0da37ac448af5da5dbb19e8b5d95702b01
yafet-123 <yafetaddisu123@gmail.com>
2024-11-29 05:44:01 -1200

    making data.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Main from '../components/home/Main'
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-        <Head>
-            <title>Yafet | Full-Stack Developer</title>
-            <meta name="description" content="I’m a Full-Stack web developer specializing in building (and occasionally designing) exceptional digital experiences." />
-            <link rel="icon" href="/fav.png" />
-        </Head>
-        <Main />
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
   )
 }

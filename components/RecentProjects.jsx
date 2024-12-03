0f4c2f5a0190014d80231b29e9c32db0219904bb
yafet-123 <yafetaddisu123@gmail.com>
2024-12-03 02:43:52 -1200

    edited the projects file in the components folder

--- components/RecentProjects.jsx
+++ components/RecentProjects.jsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
         {projects.map((item) => (
           <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]" key={item.id}>
             <PinContainer

a0b42298550165a22d5d3e98c7685133b810da78
yafet-123 <yafetaddisu123@gmail.com>
2024-12-03 03:15:17 -1200

    edited the projects file in the components folder

--- components/RecentProjects.jsx
+++ components/RecentProjects.jsx
@@ -10,14 +10,14 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
-          <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[100vw]" key={item.id}>
+          <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]" key={item.id}>
             <PinContainer
               title="Visit"
               href={item.demo}
             >
-              <div className="relative flex items-center justify-center sm:w-96 w-[100vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl">
                   <img src="/bg.png" alt="bgimg" />
                 </div>
